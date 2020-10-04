0aa71482c937e99541595946870b447a41141dee
Dan <dan.tarsoly@gmail.com>
2020-10-05 00:44:15 +0200

    Cosmetics

--- js/main.js
+++ js/main.js
@@ -2,22 +2,19 @@ const buttons = document.getElementsByTagName('button');
 const links = document.getElementsByTagName('a');
 
 for (let button of buttons) {
-    button.addEventListener('click', () => document.activeElement.blur());
+    button.addEventListener('touchend', () => document.activeElement.blur());
 }
 for (let link of links) {
-    link.addEventListener('click', () => document.activeElement.blur());
+    link.addEventListener('touchend', () => document.activeElement.blur());
 }
 
 const enBtn = document.getElementById('en-btn');
 const huBtn = document.getElementById('hu-btn');
+enBtn.onclick = () => setLanguage("en");
+huBtn.onclick = () => setLanguage("hu");
 
 let locale = navigator.language.substr(0,2);
-
-if (locale != "en") setLanguage(locale);
-
-enBtn.onclick = () => setLanguage("en");
-
-huBtn.onclick = () => setLanguage("hu");
+if (locale == "hu") setLanguage(locale);
 
 function setLanguage(lang) {
     document.querySelectorAll('body [lang]').forEach(element => {
